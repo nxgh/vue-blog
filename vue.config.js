e914231f8a138bd5c61720cd4fbd8f69e8c8aebc
nxgh <hgxn1101@gamil.com>
2019-11-20 10:15:39 +0800

    chore: :wrench: 修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,19 @@
 const path = require('path')
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
+
 module.exports = {
   lintOnSave: true,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src'))
-      .set('api', resolve('src/api'))
-      .set('assets', resolve('src/assets'))
-      .set('components', resolve('src/components'))
-      .set('layout', resolve('src/layout'))
-      .set('static', resolve('src/static'))
-      .set('views', resolve('src/views'))
-      .set('utils', resolve('src/utils'))
+  css: {
+    loaderOptions: {
+      less: {
+        jaascriptEnabled: true,
+      },
+    },
+  },
+  // publicPath: process.env.NODE_ENV === 'production' ? '/' : 'http://192.168.1.106:8000',
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, 'src/assets/css/GlobalVariable.less')],
+    },
   },
 }
